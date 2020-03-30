aafa6245479af94a49a43c1dcdb5d1de073dc3bb
mchdouglas <michaeldoug.mc@gmail.com>
2020-03-30 19:26:14 -0400

    adicionado axios

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, TextInput, Image} from 'react-native';
+
+import axios from 'axios';
 
 import styles from './styles';
 
 export default function Home() {
   const navigation = useNavigation();
 
+  const [user, setUser] = useState([]);
+  const [username, setUsername] = useState('');
+
+  const carregar = async () => {
+    const {data: result} = await axios.get(
+      `https://api.github.com/users/${username}`,
+    );
+
+    setUser(result);
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>Bem vindo primeiro que funciona.</Text>
-      </View>
+    <View style={styles.dados}>
+      <Text>Digite o usuário do github abaixo:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="github username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TouchableOpacity style={styles.btn} onPress={carregar}>
+        <Text style={styles.textBuscar}>Buscar</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.textName}>{user.name || user.login}</Text>
+
+      <Image style={styles.img} source={{uri: user.avatar_url}} />
 
       <TouchableOpacity
         style={styles.detailButton}
         onPress={() => navigation.navigate('Favorites')}>
-        <Text style={styles.detailButtonText}>Ir para home</Text>
+        <Text style={styles.detailButtonText}>Salvar nos favoritos</Text>
       </TouchableOpacity>
     </View>
   );
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -24,5 +24,42 @@ export default StyleSheet.create({
     color: '#e02041',
     fontSize: 15,
     fontWeight: 'bold',
+    marginTop: 15,
+  },
+  dados: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  input: {
+    textAlign: 'center',
+  },
+  btn: {
+    backgroundColor: '#e02041',
+    width: 90,
+    height: 30,
+    borderRadius: 7,
+    marginTop: 10,
+  },
+  textBuscar: {
+    flex: 1,
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff',
+    alignItems: 'center',
+  },
+  textName: {
+    fontSize: 15,
+    marginTop: 12,
+    marginBottom: 12,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  img: {
+    alignItems: 'center',
+    width: 280,
+    height: 280,
+    borderRadius: 900,
   },
 });
