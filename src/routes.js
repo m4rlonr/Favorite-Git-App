9ae1a6f97f37de8037d3ec20617ba30c02bf6a4c
M4rlon-R0drigues <marlon.rodrigues14@outlook.com>
2020-03-30 19:44:27 -0400

    Atualizado

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ import Home from './pages/Home';
 export default function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Favorites" component={Favorites} />
       </Stack.Navigator>
